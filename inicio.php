4f8921b8ce087823c71de837131e84382e95be22
Augustoxtmh <101999689+Augustoxtmh@users.noreply.github.com>
2023-10-24 18:40:21 -0300

    Añadicion de inicio corrected
    Corrected mi inicio añadicionado

--- inicio.php
+++ inicio.php
@@ -1,7 +1,7 @@
 <?php
-include_once("config.php");
+include_once("db_connect.php");
 
-$resultado = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
+$resultado = mysqli_query($mysqli, "SELECT * FROM user ORDER BY id DESC");
 ?>
 
 <html>
@@ -19,12 +19,12 @@ $resultado = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
             while($usuarios = mysqli_fetch_array($resultado)) {
                 echo "<tr>";
                 echo "<td style=';border-top: 1px solid black; border-left: 1px solid black; text-align: center;'>".$usuarios['usuario']."</td>";
-                echo "<td style='border-top: 1px solid black; border-left: 1px solid black; text-align: center;'>".$usuarios['contrasenia']."</td>";
+                echo "<td style='border-top: 1px solid black; border-left: 1px solid black; text-align: center;'>".$usuarios['contraseña']."</td>";
                 echo "<td style='border-top: 1px solid black; border-left: 1px solid black; text-align: center;'><a href='edit.php?id=$usuarios[id]'>Editar</a></td></tr>";
             }
             ?>
         </table>
 
-        <a href="crear.php" style="margin: 20px;" >Crear nuevo usuario</a><br/><br/>
+        <a href="create_user.html" style="margin: 20px;" >Crear nuevo usuario</a><br/><br/>
     </body>
 </html>
